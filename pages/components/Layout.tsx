fb88f0602a2b1f173e3aa836527265fb2bd1d457
Johan Chrillesen <johan.chrillesen@epitech.eu>
2024-03-11 00:16:44 -0400

    Refactor layout styles and update people grid in about page

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -7,13 +7,14 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-const styles: {[key: string]: string} = {
+const styles: { [key: string]: string } = {
   /* Default */
   // mainDiv: "relative rounded-3xs bg-base-white w-full h-[2041px] overflow-hidden text-left text-mini text-base-white font-poppins",
 
   /* Rework */
-  mainDiv: "bg-base-white flex flex-col w-full text-left text-mini text-base-white font-poppins gap-y-3",
-}
+  mainDiv:
+    "bg-base-white flex flex-col w-full text-left text-mini text-base-white font-poppins gap-y-3 min-h-screen",
+};
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
