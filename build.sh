d5970a9180299c2274570cd8fc09da8e1fc2f5bb
Julien Lafargue <julien.lafargue@epitech.eu>
2024-05-28 13:35:59 +0200

    fix the build script

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 TEMP_BUILD_DIR="temp_build"
 mkdir -p $TEMP_BUILD_DIR
 
+if ! command -v rsync &> /dev/null
+then
+    echo "rsync could not be found, installing..."
+    apt-get update && apt-get install -y rsync
+fi
+
 rsync -av --exclude='*.stories.ts' . $TEMP_BUILD_DIR --exclude="$TEMP_BUILD_DIR"
 
 cd $TEMP_BUILD_DIR
